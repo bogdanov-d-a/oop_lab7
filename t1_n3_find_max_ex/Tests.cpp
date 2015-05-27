2c0c37c284a34ab47ce68b419e968fa26f0965a9
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-05-27 21:29:04 +0300

    t1_n3_find_max_ex: add sportsmen demo

--- t1_n3_find_max_ex/Tests.cpp
+++ t1_n3_find_max_ex/Tests.cpp
@@ -19,6 +19,13 @@ private:
 	bool m_inverse;
 };
 
+struct Sportsman
+{
+	string name;
+	unsigned height;
+	unsigned weight;
+};
+
 BOOST_AUTO_TEST_CASE(TestEmptyVector)
 {
 	int maxValue;
@@ -61,4 +68,31 @@ BOOST_AUTO_TEST_CASE(TestDoubleVector)
 	double maxValue;
 	BOOST_CHECK(FindMax(vector<double>({ 1.3, 3.2, 1.9, -3.8, 2.7, 3.6 }), maxValue));
 	BOOST_CHECK_EQUAL(maxValue, 3.6);
+}
+
+BOOST_AUTO_TEST_CASE(SportsmenDemo)
+{
+	vector<Sportsman> const& sportsmen = {
+		{ "Kyle", 180, 75 },
+		{ "Bob", 195, 80 },
+		{ "James", 170, 95 }
+	};
+
+	{
+		Sportsman tallest;
+
+		BOOST_CHECK(FindMax(sportsmen, tallest,
+			[](Sportsman const& a, Sportsman const& b){ return a.height < b.height; }));
+
+		BOOST_CHECK_EQUAL(tallest.name, "Bob");
+	}
+
+	{
+		Sportsman heaviest;
+
+		BOOST_CHECK(FindMax(sportsmen, heaviest,
+			[](Sportsman const& a, Sportsman const& b){ return a.weight < b.weight; }));
+
+		BOOST_CHECK_EQUAL(heaviest.name, "James");
+	}
 }
