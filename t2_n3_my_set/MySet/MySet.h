cd6f2c2ef849d1e9f0fdcc7f5904399a61c27781
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-05-31 19:34:09 +0300

    t2_n3_my_set: set may contain elements

--- t2_n3_my_set/MySet/MySet.h
+++ t2_n3_my_set/MySet/MySet.h
@@ -4,13 +4,51 @@ template <typename T, typename Less = std::less<T>>
 class CMySet
 {
 public:
-	CMySet(Less const& less = Less())
-		:m_less(less)
+	CMySet()
+	{}
+
+	template <typename InputIterator>
+	CMySet(InputIterator const& begin, InputIterator const& end)
+	{
+		std::vector<InputIterator> iterators;
+		for (auto iterator = begin; iterator != end; ++iterator)
+		{
+			iterators.push_back(iterator);
+		}
+
+		Less less = Less();
+		std::sort(iterators.begin(), iterators.end(),
+			[&less](InputIterator const& a, InputIterator const& b){ return less(*b, *a); });
+
+		for (InputIterator iterator : iterators)
+		{
+			if (!m_start || less(*iterator, m_start->data))
+			{
+				m_start = std::make_unique<Node>(static_cast<T>(*iterator), std::move(m_start));
+			}
+		}
+	}
+
+	template <typename Container>
+	CMySet(Container const& container)
+		:CMySet(container.begin(), container.end())
 	{}
 
 	bool IsEmpty() const
 	{
 		return !m_start;
+	}
+
+	std::vector<T> ToVector() const
+	{
+		std::vector<T> result;
+
+		for (auto curNode = &m_start; *curNode; curNode = &((*curNode)->next))
+		{
+			result.push_back((*curNode)->data);
+		}
+
+		return result;
 	}
 
 private:
@@ -27,5 +65,4 @@ private:
 	};
 
 	std::unique_ptr<Node> m_start;
-	Less m_less;
 };
--- t2_n3_my_set/MySetTester/Tests.cpp
+++ t2_n3_my_set/MySetTester/Tests.cpp
@@ -1,8 +1,17 @@
 #include "stdafx.h"
 #include "../MySet/MySet.h"
 
-BOOST_AUTO_TEST_CASE(SetIsEmptyAfterConstruction)
+using namespace std;
+
+BOOST_AUTO_TEST_CASE(SetIsEmptyByDefault)
 {
 	CMySet<int> s;
 	BOOST_CHECK(s.IsEmpty());
+}
+
+BOOST_AUTO_TEST_CASE(TestNonemptySet)
+{
+	CMySet<int> s(vector<int>({ 1, 5, 2, -4, 8, 2, -4, 13 }));
+	BOOST_CHECK(!s.IsEmpty());
+	BOOST_CHECK(s.ToVector() == vector<int>({ -4, 1, 2, 5, 8, 13 }));
 }
